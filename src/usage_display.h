860783d6114c1619ea25313c24977dbaee2ecbc6
2700zc <357271164@qq.com>
2026-05-31 21:36:35 +0800

    feat: add done/failed display declarations, update thinking signature

--- src/usage_display.h
+++ src/usage_display.h
@@ -3,5 +3,7 @@
 
 void usageDisplayDraw(const UsageData& data, const char* ip = nullptr);
 void usageDisplayDrawTime(const char* ip, bool timeValid = false);
-void usageDisplayDrawThinking(uint32_t animStartMs);
+void usageDisplayDrawThinking(uint32_t elapsedMs, int step, const char* msg);
+void usageDisplayDrawDone(int steps, uint32_t elapsedMs);
+void usageDisplayDrawFailed(uint32_t elapsedMs);
 void usageDisplayDrawEaster555(uint32_t elapsedMs);
